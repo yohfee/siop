139db6784e8fcd15d77ffe94817a63b64fe7fcb0
yohfee <yohfee@gmail.com>
2015-08-20 12:05:44 +0900

    Improve consistency

--- test/test.js
+++ test/test.js
@@ -9,8 +9,19 @@ var scope = nock('http://www.data.jma.go.jp')
 
 test('generating seismic intensity observation point data', function (t) {
   siop()
-    .then(function (regions) {
-      t.equal(regions['東北地方']['宮城県']['宮城県中部'][1]['震度観測点名称'], '仙台宮城野区五輪');
+    .then(function (data) {
+      var region = data.regions[1];
+      var prefecture = region.prefectures[2];
+      var area = prefecture.areas[2];
+      var point = area.points[1];
+
+      t.equal(region.name, '東北地方');
+      t.equal(prefecture.name, '宮城県');
+      t.equal(area.name, '宮城県中部');
+      t.equal(point.name, '仙台宮城野区五輪');
+      t.equal(point.address, '仙台市宮城野区五輪1-3-15（仙台管区気象台）');
+      t.equal(point.latitude, 38.26166666666666);
+      t.equal(point.longitude, 140.89666666666668);
 
       t.end();
     })
